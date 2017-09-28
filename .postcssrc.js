59529a6c23254a10b27e35181aec15c44c084763
徐航 <xuhang2@jk.cn>
2017-09-28 11:54:34 +0800

    demo

--- .postcssrc.js
+++
@@ -1,9 +0,0 @@
-var autoprefixer = require('autoprefixer')
-var px2rem = require('postcss-px2rem')
-
-module.exports = {
-  plugins: [
-    autoprefixer({ browsers: [ '> 5%', 'last 2 versions' ] }),
-    px2rem({ remUnit: 37.5, baseDpr: 1, remPrecision: 8 })
-  ]
-}
